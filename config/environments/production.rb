eebb7a4de9c928faaf4c312a59d66185815fe95b
danielRomero <daniel.romero.esteban@gmail.com>
2013-08-01 01:48:52 +0200

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Bomberball::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+  # Heroku requires this to be false
+  config.assets.initialize_on_precompile=false
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
