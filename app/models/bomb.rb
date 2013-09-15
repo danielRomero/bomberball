d7e619de0498320909cbf065b0f45ef1a4dd744d
danielRomero <daniel.romero.esteban@gmail.com>
2013-09-15 18:26:21 +0200

    precompile assets

--- app/models/bomb.rb
+++
@@ -1,5 +0,0 @@
-class Bomb < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :game
-	belongs_to :player
-end
--- app/models/grid_elem.rb
+++
@@ -1,7 +0,0 @@
-class GridElem < ActiveRecord::Base
-	belongs_to :game
-	# => block_type can be: empty, has_bomb, has_player, brick, block
-	def get_type
-		return self.block_type+':'+self.id.to_s
-	end
-end
